d5c0ed03fa0b9a565cf2847bbb3ed0ca102f76f8
Arthurdw <arthur.dewitte@gmail.com>
2020-11-30 05:57:04 -0500

    Added install shell file

---
+++ install.sh
@@ -0,0 +1,17 @@
+export VERSION=0.0.1
+
+echo ""
+echo "Fetching phlr v${VERSION} debian package..."
+sudo wget https://github.com/Arthurdw/phlr/releases/download/v${VERSION}/phlr_${VERSION}_amd64.deb
+
+echo ""
+echo "Installing phlr v${VERSION}..."
+sudo dpkg -i phlr_${VERSION}_amd64.deb
+
+echo ""
+echo "Removing downloaded phlr v${VERSION} debian file..."
+sudo rm phlr_${VERSION}_amd64.deb
+
+echo ""
+echo "Successfully installed phlr v${VERSION}!"
+phlr --help
